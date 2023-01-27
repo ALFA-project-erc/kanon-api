fdacd3007836498ec3b10cc2ca74b2338de16a3f
Léni <lenigauffier@gmail.com>
2023-01-27 16:24:02 +0100

    fix: defered meta init leaking (#141)

--- kanon_api/tests/test_dishas_cache.py
+++ kanon_api/tests/test_dishas_cache.py
@@ -1,10 +1,13 @@
 import importlib
+import json
 import pathlib
 import shutil
 import sys
 from unittest import mock
 
 from kanon_api import settings
+from kanon_api.core.ephemerides.table_classes import TABLE_SET_DIRECTORY
+from kanon_api.core.ephemerides.tables import TableSets
 
 cache_dir = pathlib.Path("dishas_cache")
 
@@ -25,15 +28,29 @@ def test_dishas_cache():
 
     assert cache_dir.exists()
 
+    cached_tables = []
+
+    def get_int_leaves(tree):
+        if isinstance(tree, dict):
+            for value in tree.values():
+                yield from get_int_leaves(value)
+        elif isinstance(tree, int):
+            yield tree
+
+    for file in TableSets:
+        data = json.loads((TABLE_SET_DIRECTORY / f"{file.value}.json").read_text())
+        cached_tables.extend(get_int_leaves(data))
+
     files = list(cache_dir.glob("*"))
 
     assert len(files) > 0
+    assert len(files) == len(set(cached_tables))
 
     mock_open: mock.MagicMock = mock.MagicMock(wraps=open)
 
     with mock.patch("builtins.open", mock_open):
         importlib.reload(kanon_api.core.ephemerides.tables)
 
-    assert mock_open.call_count == len(files)
+    assert mock_open.call_count == len(cached_tables)
 
     settings.PRODUCTION = False
