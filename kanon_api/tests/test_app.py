9ad010f37bb92d0308e017fef799b0c9bce253aa
Ségolène Albouy <segolene.albouy@gmail.com>
2023-01-30 16:14:12 +0100

    fix: change corresponding tests

--- kanon_api/tests/test_app.py
+++ kanon_api/tests/test_app.py
@@ -14,8 +14,8 @@ client = TestClient(app)
 @pytest.mark.parametrize(
     "ts, planet, date, step, nval, result",
     [
-        ("parisian_alphonsine_tables", "sun", sdate, 1, 1, "01,48 ; 37,55"),
-        ("parisian_alphonsine_tables", "sun", (10, 2, 13), 2, 4, "05,18 ; 09,54"),
+        ("parisian_alphonsine_tables", "sun", sdate, 1, 1, "01,47 ; 18,48"),
+        ("parisian_alphonsine_tables", "sun", (10, 2, 13), 2, 4, "05,22 ; 27,44"),
         ("parisian_alphonsine_tables", "moon", sdate, 3, 1, "4,19;35,55"),
         ("parisian_alphonsine_tables", "saturn", sdate, 1, 1, "1,47;5,1"),
         ("parisian_alphonsine_tables", "venus", sdate, 1, 3, "2,1;27,13"),
@@ -185,7 +185,7 @@ def test_health_check():
 
 @pytest.mark.parametrize(
     "ts, result0, result1",
-    [("parisian_alphonsine_tables", "03,17 ; 23,48", "03,16 ; 51,51")],
+    [("parisian_alphonsine_tables", "03,16 ; 11,46", "03,15 ; 42,00")],
 )
 def test_get_ascendant(ts, result0, result1):
     response = client.get(
@@ -245,7 +245,7 @@ def test_calendars_get_infos():
 
 @pytest.mark.parametrize(
     "ts, result0, result11, result10",
-    [("parisian_alphonsine_tables", "03,17 ; 23,48", "02,47 ; 49,18", "02,16 ; 20,03")],
+    [("parisian_alphonsine_tables", "03,16 ; 11,46", "02,46 ; 34,47", "02,15 ; 00,47")],
 )
 def test_houses(ts, result0, result11, result10):
     response = client.get(
